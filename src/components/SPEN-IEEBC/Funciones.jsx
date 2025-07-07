73554549515147d2a658f4fd32c8c91d515dcf91
Roberto <xgppnqhggg5ufqk42bpaae0locifch@bots.bitbucket.org>
2025-07-06 20:46:34 -0700

    FIX primera opcion del menu

---
+++ src/components/SPEN-IEEBC/Funciones.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+{/* Por el momento tengo un diseno simple del apartado con un encabezado y pie de texto*/}
+  
+const Funciones = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold">Asignacion de Cargos</h2>
+      <p>Información sobre la Asignacion de cargos.</p>
+    </div>
+  );
+};
+
+export default Funciones;
